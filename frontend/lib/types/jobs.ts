cd04baf8ed004a20da68f2d1ee0b135d3aaa89d6
Mukul Chandrakant Mahadik <mmahadik@asu.edu>
2024-07-28 16:02:31 -0700

    Renamed directories.

--- frontend/lib/types/jobs.ts
+++
@@ -1,9 +0,0 @@
-export type JobsResponse = {
-    id: string,
-    title: string,
-    posting: string,
-};
-
-export type SelectedJobsResponse = Pick<JobsResponse,
-    'id' | 'title' | 'posting'
->;
